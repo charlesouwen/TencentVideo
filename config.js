80491154935f88a346c5c3986437c793e2488569
XSPro <win8ghost@live.com>
2021-07-24 15:43:38 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+﻿/**
+* 接口配置文件
+*
+* ！！!视频能否正常播放和播放有无广告的关键在于使用稳定有效的解析接口
+*
+* 如何获取解析接口请自行百度,开放使用的很多,也可以看下其他影视网站使用什么接口
+* 如希望播放时不呈现广告请自行寻找并使用无广告解析接口
+*
+* 接口不配置时会前往视频源站播放,如腾讯视频、爱奇艺等
+* 资源来源 https://www.gongzizx.com/
+**/
+
+jsApiConfig([
+	"http://api.aaaak.pw/wd/?v=",
+	"http://jx.popo520.cn/jiexi/?url=",
+	"http://z1.m1907.cn/?jx=",
+	"http://jx.yaohuaxuan.com/?url=",
+	"http://www.pangujiexi.cc/jiexi.php?url=",
+	"http://jx.youyitv.com/?url=",
+	"http://jx.xiaolangyun.com/?url=",
+]);
---
+++ db.php
@@ -0,0 +1,2 @@
+更多精品商业源码
+www.www.gongzizx.com
